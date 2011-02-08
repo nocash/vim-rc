f3059e72e06af724e36f87c845dc00b3e5872dd6
Beau Dacious <dacious.beau@gmail.com>
2011-02-08 09:33:22 -0500

    conditionalize submodule master checkout in update script

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-git submodule foreach '
-  git checkout master;
-  git fetch --all;
+git submodule foreach "
+  git fetch --all
+
+  GIT_BRANCH=`git branch | awk '/^* / { print $2 }'`
+  if [ ! $GIT_BRANCH = 'master' ]; then
+    git checkout master
+  fi
+
   git reset --hard origin/master
-'
+"
 
 git diff --submodule=log -- bundle/
