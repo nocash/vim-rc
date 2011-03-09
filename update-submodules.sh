b745a72f1591c33eecccb407785165306cae1304
Beau Dacious <dacious.beau@gmail.com>
2011-03-08 19:24:11 -0500

    simplify submodule update script

--- update-submodules.sh
+++ update-submodules.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-git submodule foreach "
-  git fetch --all
-
-  GIT_BRANCH=`git branch | awk '/^* / { print $2 }'`
-  if [ ! $GIT_BRANCH = 'master' ]; then
-    git checkout master
-  fi
-
-  git reset --hard origin/master
-"
-
+git submodule update
+git submodule foreach "git pull origin master"
 git diff --submodule=log -- bundle/
